9966f83a6649f27d2c0379c0f5ffc39d503c2dbe
Terje Urnes <terje@urnes.net>
2024-10-04 04:09:57 +0200

    Updating pages

--- pages-assets/index.js
+++ pages-assets/index.js
@@ -22,7 +22,7 @@ function initDemo() {
     var x = Math.floor((canvas.width - textWidth - (4 * 20)) / 2);
     ctx.fillText("210", x, 50);
 
-    ctx.fillRect(x + textWidth + 10, 36, 12, 12);
+    ctx.fillRect(x + textWidth + 10, 40, 10, 10);
 
 }
 
