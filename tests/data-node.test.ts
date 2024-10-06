05ba9d4a3c636bf0e947f9239909d97f7c737273
Terje Urnes <terje@urnes.net>
2024-10-06 18:25:46 +0200

    A few more tests

--- tests/data-node.test.ts
+++ tests/data-node.test.ts
@@ -40,20 +40,93 @@ describe("Testing new instance", () => {
 
     test("Get back the data block", () => {
         expect(instance.Get(firstKey)).toBe(dataBlock);
-    })
+    });
 
     test("Get back the first data block after key", () => {
         var smallerKey = new Key(5);    
         expect(instance.Get(smallerKey)).toBe(dataBlock);
-    })
+    });
 
     test("Get back null using a bigger key", () => {
         var biggerKey = new Key(15);
         expect(instance.Get(biggerKey)).toBeNull();
-    })
+    });
+
+    test("Has correct traverse path", () => {
+        var rapport = new TraverseRapport();
+        DataNodeEx.GetWithRapport(instance, firstKey, rapport);
+        expect(rapport.path).toStrictEqual([["10"]]);
+    });
 });
 
-describe("Linking between two blocks", () => {
+describe("Adding 10 blocks to a order 10 node", () => {
+    var instance: DataNode;
+    var dataBlocks: DataBlock[] = [];
+    var correctTraversePath = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]];
+
+    beforeEach(() => {
+        for (var i = 0; i < 10; i++) {
+            var dataBlock = new DataBlock(new Key(i + 1));
+            dataBlocks.push(dataBlock);
+        }
+        instance = new DataNode(null, 10, 1, 1, dataBlocks[0]);
+    });
+
+    test("Has correct children count", () => {
+        for (var i = 1; i < 10; i++) {
+            instance.Add(dataBlocks[i]);
+        }
+        expect(instance.ChildrenCount).toBe(10);
+    });
+
+    test("Adding nodes sequential gives correct traverse path", () => {
+        for (var i = 1; i < 10; i++) {
+            instance.Add(dataBlocks[i]);
+        }
+        CheckTraversePath();
+    });
+
+    test("Adding nodes reverse gives correct traverse path", () => {
+        for (var i = 9; i > 0; i--) {
+            instance.Add(dataBlocks[i]);
+        }
+        CheckTraversePath();
+    });
+
+    test("Adding nodes in 'random' order 1 gives correct traverse path", () => {
+        instance.Add(dataBlocks[8]);
+        instance.Add(dataBlocks[9]);
+        instance.Add(dataBlocks[5]);
+        instance.Add(dataBlocks[6]);
+        instance.Add(dataBlocks[2]);
+        instance.Add(dataBlocks[7]);
+        instance.Add(dataBlocks[4]);
+        instance.Add(dataBlocks[3]);
+        instance.Add(dataBlocks[1]);
+        CheckTraversePath();
+    });
+
+    test("Adding nodes in 'random' order 2 gives correct traverse path", () => {
+        instance.Add(dataBlocks[4]);
+        instance.Add(dataBlocks[2]);
+        instance.Add(dataBlocks[1]);
+        instance.Add(dataBlocks[6]);
+        instance.Add(dataBlocks[8]);
+        instance.Add(dataBlocks[3]);
+        instance.Add(dataBlocks[9]);
+        instance.Add(dataBlocks[5]);
+        instance.Add(dataBlocks[7]);
+        CheckTraversePath();
+    });
+
+    function CheckTraversePath() {
+        var rapport = new TraverseRapport();
+        DataNodeEx.GetWithRapport(instance, new Key(5), rapport);
+        expect(rapport.path).toStrictEqual(correctTraversePath);
+    }
+})
+
+describe("Linking between blocks", () => {
 
     var firstKey = new Key(10);
     var secondKey = new Key(20);
@@ -68,9 +141,13 @@ describe("Linking between two blocks", () => {
         instance.Add(secondDataBlock);
     });
 
-    test("Can get the second data block", () => {
+    test("2 blocks can get each others data block", () => {
         expect(dataBlock.Next).toBe(secondDataBlock);
         expect(secondDataBlock.Previous).toBe(dataBlock);
+    })
+
+    test("10 block has correct linking", () => {
+        
     })
 });
 
@@ -101,7 +178,7 @@ describe("Splitting a data node", () => {
     test("Has correct traverse paths", () => {
         instance.Add(dataBlock2);
         instance.Add(dataBlock3);
-        instance.Add(dataBlock4) as BPlusNode;
+        instance.Add(dataBlock4);
 
         var rapport = new TraverseRapport();
         DataNodeEx.GetWithRapport(instance, new Key(20), rapport);
